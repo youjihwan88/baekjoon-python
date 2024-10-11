d75fa83e6812a8690ff9114470fde12e90380253
JiHwanYou <youjihwan88@gmail.com>
2024-10-11 20:31:19 +0900

    Solve 1076: 저항

---
+++ 1076.py
@@ -0,0 +1,18 @@
+dict = {
+    "black": (0, 1),
+    "brown": (1, 10),
+    "red": (2, 100),
+    "orange": (3, 1000),
+    "yellow": (4, 10000),
+    "green": (5, 100000),
+    "blue": (6, 1000000),
+    "violet": (7, 10000000),
+    "grey": (8, 100000000),
+    "white": (9, 1000000000),
+}
+
+a = input()
+b = input()
+c = input()
+
+print((dict[a][0] * 10 + dict[b][0]) * dict[c][1])
