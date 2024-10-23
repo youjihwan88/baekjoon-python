2b4c431ff536dc0b7d451a25623047a1646f4790
JiHwanYou <youjihwan88@gmail.com>
2024-10-23 20:59:05 +0900

    Solve 23810: 골뱅이 찍기 - 뒤집힌 ㅋ

---
+++ 23810.py
@@ -0,0 +1,26 @@
+n = int(input())
+
+for _ in range(n):
+    for _ in range(n):
+        print(f"@@@@@", end="")
+    print("")
+
+for _ in range(n):
+    for _ in range(n):
+        print(f"@", end="")
+    print("")
+
+for _ in range(n):
+    for _ in range(n):
+        print(f"@@@@@", end="")
+    print("")
+
+for _ in range(n):
+    for _ in range(n):
+        print(f"@", end="")
+    print("")
+
+for _ in range(n):
+    for _ in range(n):
+        print(f"@", end="")
+    print("")
