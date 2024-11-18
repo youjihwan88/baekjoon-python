6471a014d156c2e1074a2b6e9483bddac185bdda
JiHwanYou <youjihwan88@gmail.com>
2024-11-18 19:28:19 +0900

    Solve 11279: 최대 힙

---
+++ 11279.py
@@ -0,0 +1,14 @@
+import sys
+import heapq
+
+n = int(sys.stdin.readline().rstrip())
+li = []
+for _ in range(n):
+    x = int(sys.stdin.readline().rstrip())
+    if x == 0:
+        if len(li) == 0:
+            print(0)
+        else:
+            print(-heapq.heappop(li))
+    else:
+        heapq.heappush(li, -x)
