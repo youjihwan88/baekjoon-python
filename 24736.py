f9265936e2ce66d34ff99f9f033834124b59dec6
JiHwanYou <youjihwan88@gmail.com>
2025-01-01 19:11:49 +0900

    Solve 24736: Football Scoring

---
+++ 24736.py
@@ -0,0 +1,5 @@
+a1, b1, c1, d1, e1 = map(int, input().split())
+a2, b2, c2, d2, e2 = map(int, input().split())
+
+print(a1 * 6 + b1 * 3 + c1 * 2 + d1 * 1 + e1 * 2, end=" ")
+print(a2 * 6 + b2 * 3 + c2 * 2 + d2 * 1 + e2 * 2)
